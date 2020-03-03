a7328f0588e596c76871f1fac791163e71c75213
acesaro <andrea.cesaro.mail@gmail.com>
2020-03-03 18:24:44 +0100

    Add NoExceptionCombinatorTest

--- declarser/src/test/java/kernel/stages/stage3_combinator/impl/NoExceptionCombinatorTest.java
+++ declarser/src/test/java/kernel/stages/stage3_combinator/impl/NoExceptionCombinatorTest.java
@@ -1,12 +1,15 @@
 package kernel.stages.stage3_combinator.impl;
 
 import kernel.conf.ParallelizationStrategyEnum;
+import kernel.exceptions.CombiningException;
+import kernel.exceptions.GroupedException;
 import kernel.stages.stage03_combinator.impl.NoExceptionCombinator;
 import org.junit.jupiter.api.Test;
 import utils.tryapi.Try;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,6 +20,32 @@ public class NoExceptionCombinatorTest {
     private static final class TypeT{}
     private NoExceptionCombinator<TypeK> allExceptionCombinator =
             NoExceptionCombinator.of(ParallelizationStrategyEnum.SEQUENTIAL);
+
+    /*
+     * GIVEN a map S containing entry of type (TypeK, Success)
+     * WHEN the combine method is invoked with S
+     * THEN the result is a Success
+     *  AND value is a map with all and only the S keys
+     *  AND the values are the same of S
+     */
+    @Test
+    public void when_combine_success_return_success(){
+        // GIVEN a map S containing entry of type (TypeK, Success)
+        final var sucessMap = new HashMap<TypeK, Try<?>>();
+        sucessMap.put(new TypeK(), Try.success(new TypeT()));
+        // WHEN the combine method is invoked with S
+        final var result = allExceptionCombinator.combining(sucessMap);
+        // THEN the result is a Success
+        assertTrue(result.isSuccess());
+        //  AND value is a map with all and only the S keys
+        final var valueResult = result.getValue();
+        final var keyResultSet = valueResult.keySet();
+        final var keySucess = sucessMap.keySet();
+        assertEquals(keyResultSet, keySucess);
+        //  AND the values are the same of S
+        valueResult.forEach((k,v) ->
+                assertEquals(v, sucessMap.get(k).getValue()));
+    }
 
 
     /*
@@ -33,13 +62,35 @@ public class NoExceptionCombinatorTest {
     @Test
     public void when_combine_success_and_failure_return_failure(){
         // GIVEN a map S containing entry of type (TypeK, Success)
+        final var sucessMap = new HashMap<TypeK, Try<?>>();
+        sucessMap.put(new TypeK(), Try.success(new TypeT()));
         //  AND a map F containing entry of type (TypeK, Failure) disjointed from S
+        final var failureMap = new HashMap<TypeK, Try<?>>();
+        failureMap.put(new TypeK(), Try.fail(new Exception()));
         //  LET M be the union of the S and F
+        final HashMap<TypeK, Try<?>> map = new HashMap<>();
+        map.putAll(sucessMap);
+        map.putAll(failureMap);
         // WHEN the combine method is invoked with M
+        final var result = allExceptionCombinator.combining(map);
         // THEN the result is a Failure
+        assertTrue(result.isFailure());
         //  AND Exception is of the type GroupedException
+        final var exceptionResult = result.getException();
+        assertEquals(exceptionResult.getClass(), GroupedException.class);
         //  AND all exceptions are of the type CombiningException
+        final var exceptionList = ((GroupedException) exceptionResult).getExceptions();
+        exceptionList.forEach( ex ->
+                assertEquals(ex.getClass(), CombiningException.class));
         //  AND all exceptions have a key of F and for all key of F there is an exception
+        final var combiningExceptionList = exceptionList.stream().map(ex -> ((CombiningException) ex)).collect(Collectors.toList());
+        final var resultKeySet = combiningExceptionList.stream().map(CombiningException::getKey).collect(Collectors.toSet());
+        final var failtKeySet = new HashSet<>(failureMap.keySet());
+        assertEquals(resultKeySet, failtKeySet);
         //  AND the exceptions are the same of F
+        //noinspection SuspiciousMethodCalls
+        combiningExceptionList.forEach( ex ->
+                assertEquals(ex.getCause(), failureMap.get(ex.getKey()).getException())
+        );
     }
 }
