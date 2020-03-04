c874d57818868a82e06bff6a01e74771c81ad92d
ANDREACesaro <andrea.cesaro.mail@gmail.com>
2020-03-04 11:16:03 +0100

    separate modules

--- declarser/declarser-csv/src/test/java/JUnit5ExampleTest.java
+++ declarser/declarser-csv/src/test/java/JUnit5ExampleTest.java
@@ -1,6 +1,7 @@
 import csv.CsvDeclarserFactory;
+import csv.stages.annotations.fields.CsvColumn;
 import org.junit.jupiter.api.Test;
-import samples.DataSample;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,5 +13,31 @@ class JUnit5ExampleTest {
         final var declarser = declarserFactory.declarserOf(DataSample.class, ";");
         final var dataSample = declarser.getValue().parse("Sandro;10");
         assertEquals(dataSample.getValue().getName(),"Sandro");
+        assertEquals(dataSample.getValue().getAge(),10);
     }
 }
+
+class DataSample {
+
+    @CsvColumn(0)
+    private String name;
+
+    @CsvColumn(1)
+    private Integer age;
+
+    public DataSample(
+            final String name,
+            final Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
+
