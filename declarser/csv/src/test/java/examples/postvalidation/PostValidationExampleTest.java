472416f0439d33c41ce0ca511b46728ad56f5379
acesaro <andrea.cesaro.mail@gmail.com>
2020-03-18 17:59:54 +0100

    add PostValidationExample.java

---
+++ declarser/csv/src/test/java/examples/postvalidation/PostValidationExampleTest.java
@@ -0,0 +1,57 @@
+package examples.postvalidation;
+
+import csv.CsvDeclarserFactory;
+import examples.auto.samples.AutoSimpleExample;
+import examples.postvalidation.samples.PostValidationExample;
+import kernel.validations.Validator;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostValidationExampleTest {
+
+
+    @Test
+    public void parse_valid_csv_return_success() {
+        final var csv = "10;20;50";
+        final var declarserFactory = CsvDeclarserFactory.defaultFactory();
+        final var exception = new Exception();
+        final Validator<PostValidationExample> validator = example ->
+                example.getSubQuantity1() + example.getSubQuantity2() > example.getTotalQuantity() ?
+                        Optional.of(exception) : Optional.empty();
+        final var tryDeclarser = declarserFactory.declarserOf(
+                PostValidationExample.class,
+                validator,
+                ";");
+        assertTrue(tryDeclarser.isSuccess());
+        final var declarser = tryDeclarser.getValue();
+        final var result = declarser.parse(csv);
+        assertTrue(result.isSuccess());
+        final var value = result.getValue();
+        assertEquals(value.getSubQuantity1(), 10);
+        assertEquals(value.getSubQuantity2(), 20);
+        assertEquals(value.getTotalQuantity(), 50);
+    }
+
+
+    @Test
+    public void parse_invalid_csv_return_failure() {
+        final var csv = "10;20;25";
+        final var declarserFactory = CsvDeclarserFactory.defaultFactory();
+        final var exception = new Exception();
+        final Validator<PostValidationExample> validator = example ->
+                example.getSubQuantity1() + example.getSubQuantity2() > example.getTotalQuantity() ?
+                        Optional.of(exception) : Optional.empty();
+        final var tryDeclarser = declarserFactory.declarserOf(
+                PostValidationExample.class,
+                validator,
+                ";");
+        assertTrue(tryDeclarser.isSuccess());
+        final var declarser = tryDeclarser.getValue();
+        final var result = declarser.parse(csv);
+        assertTrue(result.isFailure());
+    }
+}
+
---
+++ declarser/csv/src/test/java/examples/postvalidation/samples/PostValidationExample.java
@@ -0,0 +1,27 @@
+package examples.postvalidation.samples;
+
+import csv.stages.annotations.fields.CsvColumn;
+
+public class PostValidationExample {
+
+    @CsvColumn(0)
+    private Integer subQuantity1;
+
+    @CsvColumn(1)
+    private Integer subQuantity2;
+
+    @CsvColumn(2)
+    private Integer totalQuantity;
+
+    public Integer getSubQuantity1() {
+        return subQuantity1;
+    }
+
+    public Integer getSubQuantity2() {
+        return subQuantity2;
+    }
+
+    public Integer getTotalQuantity() {
+        return totalQuantity;
+    }
+}
